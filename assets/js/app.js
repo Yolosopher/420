28877ee3d4503310405e524a2f3e97e0b15de140
Yolosopher <nika.nishnianidze.10@gmail.com>
2021-01-04 19:58:26 +0400

    a

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,46 @@
 // variables
-const homeHeader = document.querySelector('.home-header');
-const headerContainer = document.querySelector('.header-container');
+const homeHeader = document.querySelector('.home-header')
+const headerContainer = document.querySelector('.header-container.scrolled-one')
+const headerLoginBtnFirst = document.querySelector('.header-right-login')
+const headerLoginBtns = document.querySelectorAll('.header-right-login')
 
-const burgerBtn = document.querySelector('.burger-icon');
-const burgerWindow = document.querySelector('.header-burgerMenu');
-
-
-
-
-
+const burgerBtn = document.querySelector('.burger-icon')
+const burgerWindow = document.querySelector('.header-burgerMenu')
 
 // event listeners
 window.onscroll = () => {
-    headerBotFixer();
+	headerBotFixer()
 }
 burgerBtn.addEventListener('click', () => {
-	burgerWindow.classList.toggle('active');
-	document.querySelectorAll('.moveX').forEach(obj => obj.classList.toggle('left'))
+	if (document.querySelector('.moveX.right')) {
+		headerLoginBtnFirst.click()
+	}
+	burgerWindow.classList.toggle('active')
+	document
+		.querySelectorAll('.moveX')
+		.forEach((obj) => obj.classList.toggle('left'))
 })
 
-
-
-
-
-
-
+headerLoginBtns.forEach(headerLoginBtn => {
+	headerLoginBtn.addEventListener('click', () => {
+		document
+			.querySelectorAll('.moveX')
+			.forEach((obj) => obj.classList.toggle('right'))
+	})
+})
+headerLoginBtnFirst.click()
 
 // functions
 const headerBotFixer = () => {
 	if (window.innerWidth > 1024) {
 		if (window.pageYOffset > window.innerHeight - 103) {
-            headerContainer.classList.add("scrolled");
-            // console.log(`${window.pageYOffset}  `,'chamoisqrola');
+			// headerContainer.parentElement.parentElement.classList.add("scrolled");
+			headerContainer.classList.add('scrolled')
+			// console.log(`${window.pageYOffset}  `,'chamoisqrola');
 		} else {
-			headerContainer.classList.remove("scrolled");
-            // console.log(`${window.pageYOffset}  `,'aisqrola');
+			// headerContainer.parentElement.parentElement.classList.remove("scrolled");
+			headerContainer.classList.remove('scrolled')
+			// console.log(`${window.pageYOffset}  `,'aisqrola');
 		}
 	}
 }
